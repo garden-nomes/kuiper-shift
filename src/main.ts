be0637b27567fc8d85a0141db0bb2b594ba60946
nomes <noahbweiner@gmail.com>
2021-02-01 17:34:23 -0800

    math out all three dimensions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,44 @@
 import { init, TextAlign, VerticalAlign } from "pota-8";
 import fontSrc from "../assets/Gizmo199lightfont.png";
+import {
+  addVec,
+  mult3x3,
+  mult3x3vec,
+  pitchMatrix,
+  projectToScreen,
+  scaleVec,
+  viewMatrix,
+  yawMatrix
+} from "./math";
 
 const light = [199, 240, 216];
 const dark = [67, 82, 61];
 
+const asteroids: number[][] = [];
+
+const ship = {
+  rot: [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ],
+  pos: [0, 0, 0],
+  vel: [0, 0, 0]
+};
+
+function turn(pitch: number, yaw: number) {
+  ship.rot = mult3x3(pitchMatrix(pitch), mult3x3(yawMatrix(yaw), ship.rot));
+}
+
+function accelerate(acc: number[]) {
+  ship.vel = addVec(ship.vel, mult3x3vec(ship.rot, acc));
+}
+
+let frame = 0;
+
 init({
+  showFps: import.meta.env.DEV,
+
   dimensions: [84, 48],
   crop: true,
 
@@ -15,17 +49,40 @@ init({
     letters: "!\"# % '()*+,-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz"
   },
 
-  setup() {},
+  setup() {
+    for (let i = 0; i < 100; i++) {
+      asteroids.push([
+        Math.random() * 10 - 5,
+        Math.random() * 10 - 5,
+        Math.random() * 10 - 5
+      ]);
+    }
+  },
 
   loop() {
-    const [bg, text] = p.elapsed % 2 > 1 ? [light, dark] : [dark, light];
+    p.clear(dark);
 
-    p.clear(bg);
+    if (p.keyDown("left")) turn(0, p.deltaTime);
+    if (p.keyDown("right")) turn(0, -p.deltaTime);
+    if (p.keyDown("up")) turn(p.deltaTime, 0);
+    if (p.keyDown("down")) turn(-p.deltaTime, 0);
 
-    p.text("nokia 3310 jam", p.width / 2, p.height / 2, {
-      color: text,
-      align: TextAlign.Center,
-      verticalAlign: VerticalAlign.Middle
-    });
+    if (p.keyDown("z")) accelerate([0, 0, p.deltaTime]);
+    if (p.keyDown("x")) accelerate([0, 0, -p.deltaTime]);
+
+    ship.vel = scaleVec(ship.vel, 1 - p.deltaTime * 2);
+    ship.pos = addVec(ship.pos, scaleVec(ship.vel, p.deltaTime));
+
+    const view = viewMatrix(ship.pos, ship.rot);
+    for (const [x, y, z] of asteroids) {
+      const [sx, sy, sz] = projectToScreen(view, x, y, z);
+      if (sz > 0) {
+        const r = 8 / (sz + 0.01);
+
+        if (sx + r > 0 && sx - r < p.width && sy + r > 0 && sy - r < p.height) {
+          p.circle(sx, sy, 8 / (sz + 0.01), light);
+        }
+      }
+    }
   }
 });
---
+++ src/math.ts
@@ -0,0 +1,92 @@
+function dot(v0: number[], v1: number[]) {
+  return v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2];
+}
+
+export function mult3x3(m0: number[][], m1: number[][]) {
+  return [
+    [
+      m0[0][0] * m1[0][0] + m0[0][1] * m1[1][0] + m0[0][2] * m1[2][0],
+      m0[0][0] * m1[0][1] + m0[0][1] * m1[1][1] + m0[0][2] * m1[2][1],
+      m0[0][0] * m1[0][2] + m0[0][1] * m1[1][2] + m0[0][2] * m1[2][2]
+    ],
+    [
+      m0[1][0] * m1[0][0] + m0[1][1] * m1[1][0] + m0[1][2] * m1[2][0],
+      m0[1][0] * m1[0][1] + m0[1][1] * m1[1][1] + m0[1][2] * m1[2][1],
+      m0[1][0] * m1[0][2] + m0[1][1] * m1[1][2] + m0[1][2] * m1[2][2]
+    ],
+    [
+      m0[2][0] * m1[0][0] + m0[2][1] * m1[1][0] + m0[2][2] * m1[2][0],
+      m0[2][0] * m1[0][1] + m0[2][1] * m1[1][1] + m0[2][2] * m1[2][1],
+      m0[2][0] * m1[0][2] + m0[2][1] * m1[1][2] + m0[2][2] * m1[2][2]
+    ]
+  ];
+}
+
+export function mult3x3vec(m: number[][], v: number[]) {
+  return [
+    m[0][0] * v[0] + m[1][0] * v[1] + m[2][0] * v[2],
+    m[0][1] * v[0] + m[1][1] * v[1] + m[2][1] * v[2],
+    m[0][2] * v[0] + m[1][2] * v[1] + m[2][2] * v[2]
+  ];
+}
+
+export function addVec(v0: number[], v1: number[]) {
+  return [v0[0] + v1[0], v0[1] + v1[1], v0[2] + v1[2]];
+}
+
+export function scaleVec(v: number[], s: number) {
+  return [v[0] * s, v[1] * s, v[2] * s];
+}
+
+export function rollMatrix(yaw: number) {
+  const c = Math.cos(yaw);
+  const s = Math.sin(yaw);
+
+  return [
+    [c, -s, 0],
+    [s, c, 0],
+    [0, 0, 1]
+  ];
+}
+
+export function yawMatrix(yaw: number) {
+  const c = Math.cos(yaw);
+  const s = Math.sin(yaw);
+
+  return [
+    [c, 0, s],
+    [0, 1, 0],
+    [-s, 0, c]
+  ];
+}
+
+export function pitchMatrix(yaw: number) {
+  const c = Math.cos(yaw);
+  const s = Math.sin(yaw);
+
+  return [
+    [1, 0, 0],
+    [0, c, -s],
+    [0, s, c]
+  ];
+}
+
+export function viewMatrix(pos: number[], rot: number[][]) {
+  return [
+    [rot[0][0], rot[1][0], rot[2][0], 0],
+    [rot[0][1], rot[1][1], rot[2][1], 0],
+    [rot[0][2], rot[1][2], rot[2][2], 0],
+    [-dot(rot[0], pos), -dot(rot[1], pos), -dot(rot[2], pos), 1]
+  ];
+}
+
+export function projectToScreen(viewMatrix: number[][], x: number, y: number, z: number) {
+  [x, y, z] = [
+    viewMatrix[0][0] * x + viewMatrix[1][0] * y + viewMatrix[2][0] * z + viewMatrix[3][0],
+    viewMatrix[0][1] * x + viewMatrix[1][1] * y + viewMatrix[2][1] * z + viewMatrix[3][1],
+    viewMatrix[0][2] * x + viewMatrix[1][2] * y + viewMatrix[2][2] * z + viewMatrix[3][2]
+  ];
+
+  const s = p.width / 2;
+  return [(x * s) / z + p.width / 2, (y * -s) / z + p.height / 2, z];
+}
